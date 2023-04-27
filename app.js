5cfef3307532101a8729bd77e8ee16e397d12955
nessarz93 <126210620+nessarz93@users.noreply.github.com>
2023-04-27 02:56:57 +0000

    Agregado dolares a yen

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const sum = (a,b) => {
 }
 
 // just a console log for ourselves.
-console.log(sum(7,3))
+console.log(sum(7,3));
 
-// export the function to be used on other files 
-// (similar to the keyword `export` when using webpack)
-module.exports = { sum };
+
+
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = valueInDollar * 1.2;
+    return valueInYen;
+}
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen }
